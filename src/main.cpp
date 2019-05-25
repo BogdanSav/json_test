7d09e66fcd9ce9e1e98c1310484d969e1d5e1c14
BogdanSav <bohdan16@live.com>
2019-05-25 20:36:45 +0300

    full_json

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,45 @@
 #include <ArduinoJson.h>
 
 
-
 void setup()
 {
-StaticJsonDocument<200> doc;
+StaticJsonDocument<900> doc;
 String Jsondoc;
 doc["deviceId"] = 1;
 JsonArray readers = doc.createNestedArray("readers");
-JsonObject reader = readers.createNestedObject();
-JsonObject reader1 = readers.createNestedObject();
-JsonArray items = readers.createNestedArray();
-JsonObject item1 = items.createNestedObject();
-JsonObject item2 = items.createNestedObject();
-item1["rfid"] = "awdawdadadaw";
-item1["rfid"] = "5684658555";
 
-reader["item"] = item1;
-reader1["item"] = item2;
+
+JsonObject readerFirst = readers.createNestedObject();
+readerFirst["readerId"] = "994";
+JsonArray firstItemArray = readerFirst.createNestedArray("items");
+JsonObject innerFirstItem = firstItemArray.createNestedObject();
+innerFirstItem["rfid"] = "firstValue rfid";
+
+JsonObject readerSecond = readers.createNestedObject();
+readerSecond["readerId"] = "261";
+JsonArray secondItemArray = readerSecond.createNestedArray("items");
+JsonObject innerSecondaryItem = secondItemArray.createNestedObject();
+innerSecondaryItem["rfid"] = "second rfidValue";
+
+JsonObject readerThird = readers.createNestedObject();
+readerThird["readerId"] = "222";
+JsonArray thirdItemArray = readerThird.createNestedArray("items");
+JsonObject innerThirdItem = thirdItemArray.createNestedObject();
+innerThirdItem["rfid"] = "third rfidValue";
+
+JsonObject readerFour = readers.createNestedObject();
+readerFour["readerId"] = "4444";
+JsonArray fourItemArray = readerFour.createNestedArray("items");
+JsonObject innerFourItem = fourItemArray.createNestedObject();
+innerFourItem["rfid"] = "Four rfidValue";
+
 
 serializeJson(doc,Jsondoc);
 Serial.begin(115200);
+Serial.println("---------------------------");
 Serial.println(Jsondoc);
 
 }
 void loop()
 {
-
-
-
 }
